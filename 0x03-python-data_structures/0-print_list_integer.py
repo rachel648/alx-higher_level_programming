0340832dd4636ad1c508e6393107b7ad5adb6861
rachel648 <rachelraqelmurithi@gmail.com>
2023-08-14 02:39:24 -0700

    h

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
-def print_list_integer(my_list=[]):
-    for n in my_list:
-        print("{:d}".format(n))
+def no_c(my_string):
+    new_string = ""
+    for c in my_string:
+        if c not in "Cc":
+            new_string += c
+    return new_string
