64bb77870d9e479e9a8a820601f3ded20f585bee
rachel648 <rachelraqelmurithi@gmail.com>
2023-08-17 10:03:42 -0700

    k

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of elements in a linked dlistint_t list
+ * @h: pointer to the first member of a dlistint_t list
+ * Return: size_t amount of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+const dlistint_t *temp = h;
+size_t nodes;
+
+for (nodes = 0; temp; nodes++)
+{
+temp = temp->next;
+}
+return (nodes);
+}
---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds new node at the end of a dlistint_t list
+ * @head: double pointer to the first member of a dlistint_t list
+ * @n: int value stored in new node.
+ * Return: address the new element, or NULL if it failed
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+dlistint_t *new, *tail;
+new = malloc(sizeof(dlistint_t));
+if (!new)
+{
+dprintf(2, "Error: Can't malloc\n");
+return (NULL);
+}
+tail = *head;
+if (!tail)
+*head = new;
+else
+{
+while (tail->next)
+tail = tail->next;
+tail->next = new;
+}
+
+new->n = n;
+new->prev = tail;
+new->next = NULL;
+
+return (new);
+}
