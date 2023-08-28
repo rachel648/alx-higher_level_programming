73e91050832fb6280fdf0b0a06e0d19b6809c31e
rachel648 <rachelraqelmurithi@gmail.com>
2023-08-28 12:16:25 -0700

    0

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+def safe_function(fct, *args):
+    try:
+        r = fct(*args)
+        return r
+    except Exception as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+        return None
