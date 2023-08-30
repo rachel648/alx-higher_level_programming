6111fda75024891b29d0aab6ec4732a79dd1ac1f
rachel648 <rachelraqelmurithi@gmail.com>
2023-08-30 00:48:01 +0000

    l

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,12 @@
-re class definition"""
+#!/usr/bin/python3
+"""class Square that defines a square"""
 
+class Square():
+    """square class with it's size and proper validation"""
 
-class Square:
-    """Represents a square
-    Attributes:
-        __size (int): size of a side of the square
-    """
-    def __init__(self, size):
-        """Initializes a square
-        Args:
-            size (int): size of a side of the square
-        Returns: None
-        """
+    def __init__(self, size=0):
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
         self.__size = size
